62f223b2fcb10b94c252fa785bda3f9c65397079
Joshua Liew <liqshann@gmail.com>
2025-10-08 22:00:41 +0900

    l14: update: interesting solution

--- ch4/lesson14.py
+++ ch4/lesson14.py
@@ -4,9 +4,10 @@ def find_longest_word(document, longest_word=""):
 
     if " " in document:
         first_word, document = document.split(maxsplit=1)
+        longest_word = find_longest_word(document, longest_word)
     else:
         first_word, document = document, None
 
-    if len(first_word) > len(longest_word):
+    if len(first_word) >= len(longest_word):
         longest_word = first_word
-    return find_longest_word(document, longest_word) if document else longest_word
+    return longest_word
