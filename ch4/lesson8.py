6b4637bd13f52ab09778a5fc9a9602ff5eb264d7
Joshua Liew <liqshann@gmail.com>
2025-10-08 21:01:49 +0900

    l8: cleaner solution

--- ch4/lesson8.py
+++ ch4/lesson8.py
@@ -1,7 +1,9 @@
 def sum_nested_list(lst):
     if len(lst) == 0:
         return 0
-    if type(lst[0]) is list:
-        return sum_nested_list(lst[0]) + sum_nested_list(lst[1:])
 
-    return lst[0] + sum_nested_list(lst[1:])
+    if type(lst[0]) is list:
+        val = sum_nested_list(lst[0])
+    else:
+        val = lst[0]
+    return val + sum_nested_list(lst[1:])
